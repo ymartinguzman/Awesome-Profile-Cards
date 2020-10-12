3667db4588546eccd6a3940469e9d044eb194506
martreyz <martreyz@gmail.com>
2020-10-12 09:17:53 +0200

    Add js to form menu

--- src/js/main.js
+++ src/js/main.js
@@ -20,7 +20,7 @@ function fakeFileClick() {fileField.click(); }
 uploadBtn.addEventListener('click', fakeFileClick);
 fileField.addEventListener('change', getImage);
 
-
+// Form JS
 const buttonDesig = document.querySelector('.customize-design__container-btn');
 const buttonFill = document.querySelector('.customize-menu__container-btn');
 const buttonShare = document.querySelector('.customize-share__container-btn');
@@ -30,25 +30,41 @@ const sectionFill = document.querySelector('.customize-menu__form');
 const sectionShare = document.querySelector('.gen-main__customize-share--hidden');
 const createCardBtn = document.querySelector('.customize-share__button');
 
+
+
+function createCardEvent (event) {
+  createCardBtn.classList.add('js-cardCreated');
+  sectionShare.classList.remove('js__hiddenlink');
+  sectionShare.classList.remove('js__hidden');
+  event.preventDefault();
+}
+createCardBtn.addEventListener('click', createCardEvent);
+
 function hideSection(event){
   if (event.currentTarget === buttonDesig){
     sectionDesig.classList.toggle('js__hidden');
     sectionFill.classList.add('js__hidden');
     sectionShare.classList.add('js__hidden');
     createCardBtn.classList.add('js__hidden');
-    buttonDesig.classList.toggle('.js__button-arrow');
+    buttonDesig.classList.toggle('js__button-arrow');
+    buttonFill.classList.remove('js__button-arrow');
+    buttonShare.classList.remove('js__button-arrow');
   } else if (event.currentTarget === buttonFill){
     sectionFill.classList.toggle('js__hidden');
     sectionShare.classList.add('js__hidden');
     createCardBtn.classList.add('js__hidden');
     sectionDesig.classList.add('js__hidden');
-    buttonFill.classList.toggle('.js__button-arrow');
+    buttonDesig.classList.remove('js__button-arrow');
+    buttonFill.classList.toggle('js__button-arrow');
+    buttonShare.classList.remove('js__button-arrow');
   } else if (event.currentTarget === buttonShare){
     sectionShare.classList.toggle('js__hidden');
     createCardBtn.classList.toggle('js__hidden');
     sectionDesig.classList.add('js__hidden');
     sectionFill.classList.add('js__hidden');
-    buttonShare.classList.toggle('.js__button-arrow');
+    buttonDesig.classList.remove('js__button-arrow');
+    buttonShare.classList.toggle('js__button-arrow');
+    buttonFill.classList.remove('js__button-arrow');
   }
 }
 
