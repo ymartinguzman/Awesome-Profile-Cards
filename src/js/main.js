6a7b3a503cbbf01d863f16d9f126ba47d87dc0b7
Estibaliz Barato <estibalizbarato@gmail.com>
2020-10-16 14:38:28 +0200

    inital commit and better code js gen

--- src/js/main.js
+++ src/js/main.js
@@ -1,81 +1,49 @@
-'use strict';
+"use strict";
 //BOTON AÑADIR IMAGEN
 
 const fr = new FileReader();
-const uploadBtn = document.querySelector('.js__box-button');
-const fileField = document.querySelector('.js__box-button-hidden');
-const profileImage = document.querySelector('.js__profile-image');
-const profilePreview = document.querySelector('.js__profile-preview');
+const uploadBtn = document.querySelector(".js__box-button");
+const fileField = document.querySelector(".js__box-button-hidden");
+const profileImage = document.querySelector(".js__profile-image");
+const profilePreview = document.querySelector(".js__profile-preview");
 
-
-function getImage(e){
+function getImage(e) {
   const myFile = e.currentTarget.files[0];
-  fr.addEventListener('load', writeImage);
+  fr.addEventListener("load", writeImage);
   fr.readAsDataURL(myFile);
 }
 
-
 function writeImage() {
-
   profileImage.style.backgroundImage = `url(${fr.result})`;
   profilePreview.style.backgroundImage = `url(${fr.result})`;
 }
 
+function fakeFileClick() {
+  fileField.click();
+}
 
-function fakeFileClick() {fileField.click(); }
+uploadBtn.addEventListener("click", fakeFileClick);
+fileField.addEventListener("change", getImage);
 
-
-uploadBtn.addEventListener('click', fakeFileClick);
-fileField.addEventListener('change', getImage);
-
-'use strict';
+("use strict");
 //DESPLEGABLES
 
-const buttonDesig = document.querySelector('.customize-design__container-btn');
-const buttonFill = document.querySelector('.customize-menu__container-btn');
-const buttonShare = document.querySelector('.customize-share__container-btn');
+const buttonContainer = document.querySelectorAll(".js-collapsable");
+const sectionContainer = document.querySelectorAll(".js-section");
+const buttonArrow = document.querySelectorAll(".js-arrow");
 
-const sectionDesig = document.querySelector('.customize-design__colors');
-const sectionFill = document.querySelector('.customize-menu__form');
-const sectionShare = document.querySelector('.gen-main__customize-share--hidden');
-const createCardBtn = document.querySelector('.customize-share__button');
-
-function createCardEvent (event) {
-  createCardBtn.classList.add('js-cardCreated');
-  sectionShare.classList.remove('js__hiddenlink');
-  sectionShare.classList.remove('js__hidden');
-  event.preventDefault();
-}
-createCardBtn.addEventListener('click', createCardEvent);
-
-function hideSection(event){
-  if (event.currentTarget === buttonDesig){
-    sectionDesig.classList.toggle('js__hidden');
-    sectionFill.classList.add('js__hidden');
-    sectionShare.classList.add('js__hidden');
-    createCardBtn.classList.add('js__hidden');
-    buttonDesig.classList.toggle('js__button-arrow');
-    buttonFill.classList.remove('js__button-arrow');
-    buttonShare.classList.remove('js__button-arrow');
-  } else if (event.currentTarget === buttonFill){
-    sectionFill.classList.toggle('js__hidden');
-    sectionShare.classList.add('js__hidden');
-    createCardBtn.classList.add('js__hidden');
-    sectionDesig.classList.add('js__hidden');
-    buttonDesig.classList.remove('js__button-arrow');
-    buttonFill.classList.toggle('js__button-arrow');
-    buttonShare.classList.remove('js__button-arrow');
-  } else if (event.currentTarget === buttonShare){
-    sectionShare.classList.toggle('js__hidden');
-    createCardBtn.classList.toggle('js__hidden');
-    sectionDesig.classList.add('js__hidden');
-    sectionFill.classList.add('js__hidden');
-    buttonDesig.classList.remove('js__button-arrow');
-    buttonShare.classList.toggle('js__button-arrow');
-    buttonFill.classList.remove('js__button-arrow');
+function collapseMenu(ev) {
+  for (let i = 0; i < buttonContainer.length; i++) {
+    if (ev.currentTarget === buttonContainer[i]) {
+      sectionContainer[i].classList.toggle("js__hidden");
+      buttonArrow[i].classList.toggle("js__button-arrow");
+      // console.log("jola");
+    } else {
+      sectionContainer[i].classList.add("js__hidden");
+      buttonArrow[i].classList.remove("js__button-arrow");
+    }
   }
 }
-
-buttonDesig.addEventListener('click', hideSection);
-buttonFill.addEventListener('click', hideSection);
-buttonShare.addEventListener('click', hideSection);
+for (const buttonCollapse of buttonContainer) {
+  buttonCollapse.addEventListener("click", collapseMenu);
+}
